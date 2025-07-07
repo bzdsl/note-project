9aed70d9aec6661b50a902496d3927d520b45a69
Thang Duc Nguyen <75111229+bzdsl@users.noreply.github.com>
2025-07-07 11:14:13 +0700

    changed file name

--- backend/src/routes/noteRoutes.js
+++ backend/src/routes/noteRoutes.js
@@ -6,6 +6,7 @@ import {
   createNewNote,
   updateNote,
   deleteNote,
+  getNote,
 } from "../controllers/notesController.js";
 const router = express.Router();
 
@@ -14,6 +15,6 @@ router.get("/", getAllNotes);
 router.post("/", createNewNote);
 
 router.put("/:id", updateNote);
-
+router.get("/:id", getNote);
 router.delete("/:id", deleteNote);
 export default router;
